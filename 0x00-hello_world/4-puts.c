08e4dc2953788095c0394d503e4c4d966ee30c74
ali007-depug <aliroma849@gamil.com>
2022-10-14 07:53:22 -0700

    done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-/*
- * this is main functionn
- *
- * */
+/**
+ * main-it's main function,main
+ * puts: use for print strings 
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
