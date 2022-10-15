70ec69f6bca3bc8c35e00fb4a2d3791c6b5ba6b2
ali007-depug <aliroma849@gamil.com>
2022-10-15 10:24:38 -0700

    done

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+nclude <stdio.h>
+/**
+ * main - main function
+ *
+ * Return:0
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++){
+	if (ch != 'q' &&  ch != 'e' )
+	putchar(ch);
+	}	    
+	putchar('\n');
+	return (0);
+}
+
