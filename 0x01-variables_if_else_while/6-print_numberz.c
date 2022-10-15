7ee4b73cf74976cd5664bbc41c997f44e54c424c
ali007-depug <aliroma849@gamil.com>
2022-10-15 13:05:05 -0700

    done

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,9 +7,11 @@
 
 int main(void)
 {
-	for(char n = '0' ; n <= '9' ; n++)
-	putchar(n);
-        
+	int num;
+
+	for (num = 0; num < 10; num++)
+	putchar((num % 10) + '0');
+
 	putchar('\n');
 	return (0);
 }
