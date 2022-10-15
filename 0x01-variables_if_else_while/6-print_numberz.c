936807bd6ed3b68c734933aeb941561e5347dc8b
ali007-depug <aliroma849@gamil.com>
2022-10-15 11:02:55 -0700

    done

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return:0
+ */
+
+int main(void)
+{
+	for(char n = '0' ; n <= '9' ; n++)
+	putchar(n);
+        
+	putchar('\n');
+	return (0);
+}
