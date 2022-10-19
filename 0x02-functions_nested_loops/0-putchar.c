8c50d49bf7d0b20f7d142744643cc0e53f943570
ali007-depug <aliroma849@gamil.com>
2022-10-19 15:05:48 -0700

    done

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <_putchar.h>
+#include "_putchar.h"
 /**
  * main- main function
  *
@@ -18,6 +18,6 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
-
+	_putchar('\n');
 	return(0);
 }
